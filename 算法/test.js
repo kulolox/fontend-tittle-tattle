820d7449b9e4b21416396b403930195cba1ea34c
xujianfeng <1048016718@qq.com>
2020-11-23 14:01:30 +0800

    执行上下文笔记

--- 算法/test.js
+++ 算法/test.js
@@ -1,18 +1,8 @@
-function sort(arr) {
-  let len = arr.length - 1;
-  while (len > 0) {
-    let pos = 0;
-    for (let j = 0; j < len; j++) {
-      if (arr[j] > arr[j + 1]) {
-        pos = j;
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
-    }
-    len = pos;
-  }
-  return arr;
+function foo(a) {
+  console.log('1:', a);
+
+  var a = 2;
+  console.log('2:', a);
 }
 
-const arr = [2, 5, 10, 7, 10, 32, 90, 9, 11, 1];
-
-console.log(sort(arr));
+foo(1);
